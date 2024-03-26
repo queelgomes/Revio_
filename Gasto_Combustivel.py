37f9430823dd6fe009bd8b061d7eae0b4fd5c948
queelgomes <quel.4@hotmail.com>
2024-03-26 16:21:06 -0300

    Testes REVIO

---
+++ Gasto_Combustivel.py
@@ -0,0 +1,5 @@
+hours = int(input())
+speed = int(input())
+result = float((speed / 12) * hours)
+
+print(f"{result:,.3f}")
---
+++ Salario_Com_Bonus.py
@@ -0,0 +1,6 @@
+name = str(input()).split()
+salary = float(input())
+total_value = float(input())
+result = (total_value * 0.15) + salary
+
+print(f"TOTAL = R$ {result:.2f}")
