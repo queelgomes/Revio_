a548d49e505c01231b57cc9d135c4b8fdcf2fa85
Queel Gomes <124888102+queelgomes@users.noreply.github.com>
2024-03-26 22:34:27 -0300

    Add files via upload

---
+++ Testes_SQL.sql
@@ -0,0 +1,15 @@
+-- Endereço dos CLientes
+SELECT name, street FROM customers WHERE city = 'Porto Alegre'
+
+-- Maior e Menor Preço
+SELECT MAX(price), MIN(price) FROM products;
+
+-- Pessoas Juridicas
+SELECT name FROM customers WHERE id IN (SELECT id_customers FROM legal_person);
+
+-- Filmes em Promoção
+SELECT id, name FROM movies WHERE id_prices in (SELECT id FROM prices WHERE value < 2.00)
+
+-- Quantidades Entre 10 e 20
+SELECT name FROM product WHERE amount BETWEEN 10 AND 20 AND id_providers in (SELECT id FROM providers WHERE name LIKE "P%")
+
